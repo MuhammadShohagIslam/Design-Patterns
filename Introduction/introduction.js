3fdb5ac1e1ffd73aabb52650749da9d58c0b1e37
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-08-11 09:21:37 +0300

    done introduction design patterns

---
+++ Introduction/introduction.js
@@ -0,0 +1,10 @@
+/*
+    *** Design-Patterns ***
+        => Desigsn patterns are a fundemental parts of software development for this reason, it is provided typical solutions to commonly repeating problems in software design.
+        => We can use design patterns for avoiding reapeting code and optimize way to manage code.
+        => It help promote easier program changes and object resuability.
+        => Loosely coupled objects are easier to resude and change.
+        => Design patterns are built with small specialized objects.
+
+        Credit by Patterns and techrepublich website.
+ */
