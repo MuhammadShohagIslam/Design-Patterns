26c25c5a9270d569c16bfac7c930fae3a82ae753
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-08-19 20:49:09 +0300

    Done KISS Principles

---
+++ KISS-Principle/KISS-Principle.js
@@ -0,0 +1,37 @@
+/*
+    *** KISS Principles ***
+        => KISS means Keep It Simple Stupid. It does not mean that you
+        are studpid. You are very smart and beautiful.
+        => It means that our code should be studpidly simple. It should be so simple, everybody should understand what the system does 
+        and grasp quickly as possible a good mental model of it.
+        => It should be so simple that when we will quit our company , a
+        junior developer should be able to continue our masterpiece.
+
+        *** Simple ***
+            => A Simple system is not a system with one and only one
+            element necessarily. It's system which has not too many
+            different elements connected to each other.
+        
+        *** To summarize, here are the two sign of complexity:
+            => Too many parts in the system.
+            => Too many iterconnected part in the system.
+
+        *** Business Complexity ***
+            => The complexity of a system will also be a good indicator
+            of the amount of time developers need to add a new functionality.
+
+            *** As a developer, we need to keep the flowing in mind.
+                => If we can avoid complexity, avoid it, as much as we can.
+                => If we can not, we need to make sure that we manage it properly.
+                => To avoid as much buiness complexity as possibile, i 
+                suggest at tacking the complexity of the buiness requirement directly.
+
+        *** A good implemetation is Deleted One ***
+
+            => Less code will often make our system simpler. Big is often synoym with complexity.
+            => Intead, changing our code can have an impact on the surrounding code and create undesired results.
+            => Simplifying our codebase by removing dead code will simplify our mental models.
+
+            
+    Credit By thevaluable.dev
+*/
