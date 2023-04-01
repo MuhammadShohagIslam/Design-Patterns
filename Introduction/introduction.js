5a5930bf41db3216f26d5fd74bab1963f47aae15
Md Jhohirul Islam <91692259+MuhammadShohagIslam@users.noreply.github.com>
2023-04-01 20:14:08 +0300

    update introduction

--- Introduction/introduction.js
+++ Introduction/introduction.js
@@ -1,6 +1,7 @@
 /*
     *** Design-Patterns ***
-        => Desigsn patterns are a fundemental parts of software development for this reason, it is provided typical solutions to commonly repeating problems in software design.
+        => Design pattern is a proven and reusable to a common problem in software design.
+        => Desigsn patterns are a fundemental parts of software development, it helps us to solve common repeating problems in software design.
         => We can use design patterns for avoiding reapeting code and optimize way to manage code.
         => It help promote easier program changes and object resuability.
         => Loosely coupled objects are easier to resude and change.
@@ -8,3 +9,8 @@
 
         Credit by Patterns and techrepublich website.
  */
+/*
+   *** Advantage of Design Patterns***
+      => design pattern helps write higher quality code, which easier to maintain, readable, extended.
+      => which increase prodcutivity, better perfomance. facilates teamwork.
+*/
