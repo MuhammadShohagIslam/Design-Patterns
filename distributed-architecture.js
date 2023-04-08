27f8c0b8b82fc9052eeb3eff599000c993ec38ea
Md Jhohirul Islam <91692259+MuhammadShohagIslam@users.noreply.github.com>
2023-04-08 12:19:54 +0300

    distributed system

---
+++ distributed-architecture.js
@@ -0,0 +1,6 @@
+/*
+  *** Why you need distribution ***
+    => The speciality of node architecture: single-thread
+    => CPU intensive and time intensive work: Video Convertion
+    => Retry and notification
+*/
