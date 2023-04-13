6404ad43ac46adceb37bfa56d326986232e7e0f1
Md Jhohirul Islam <91692259+MuhammadShohagIslam@users.noreply.github.com>
2023-04-13 14:48:10 +0300

    intro

--- design-system.js
+++ design-system.js
@@ -1,1 +1,5 @@
-
+/*
+  *** A design system is a set of interconnected pattern, share practices with organise inter-connected pattern.
+  *** Design pattern, we can see any kind of intersprise project, it is the indetifies of the brand
+  *** help save time.
+*/
